72b6b1641596013e9475cd24c1f489de7c7f83b7
Rasmusson <emma.rasmusson@accenture.com>
2020-01-13 16:53:01 +0100

    Generated a new smart contract project

---
+++ backend/utils/SmartContract/lib/food-contract.js
@@ -0,0 +1,56 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+
+class FoodContract extends Contract {
+
+    async foodExists(ctx, foodId) {
+        const buffer = await ctx.stub.getState(foodId);
+        return (!!buffer && buffer.length > 0);
+    }
+
+    async createFood(ctx, foodId, value) {
+        const exists = await this.foodExists(ctx, foodId);
+        if (exists) {
+            throw new Error(`The food ${foodId} already exists`);
+        }
+        const asset = { value };
+        const buffer = Buffer.from(JSON.stringify(asset));
+        await ctx.stub.putState(foodId, buffer);
+    }
+
+    async readFood(ctx, foodId) {
+        const exists = await this.foodExists(ctx, foodId);
+        if (!exists) {
+            throw new Error(`The food ${foodId} does not exist`);
+        }
+        const buffer = await ctx.stub.getState(foodId);
+        const asset = JSON.parse(buffer.toString());
+        return asset;
+    }
+
+    async updateFood(ctx, foodId, newValue) {
+        const exists = await this.foodExists(ctx, foodId);
+        if (!exists) {
+            throw new Error(`The food ${foodId} does not exist`);
+        }
+        const asset = { value: newValue };
+        const buffer = Buffer.from(JSON.stringify(asset));
+        await ctx.stub.putState(foodId, buffer);
+    }
+
+    async deleteFood(ctx, foodId) {
+        const exists = await this.foodExists(ctx, foodId);
+        if (!exists) {
+            throw new Error(`The food ${foodId} does not exist`);
+        }
+        await ctx.stub.deleteState(foodId);
+    }
+
+}
+
+module.exports = FoodContract;
