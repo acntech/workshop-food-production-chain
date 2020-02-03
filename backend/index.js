15a3ea9dbe675812e679bfe5ba34e1efea7a7f84
Rasmusson <emma.rasmusson@accenture.com>
2020-02-03 15:09:28 +0100

    Mocked responses from API

--- backend/index.js
+++ backend/index.js
@@ -42,53 +42,62 @@ app.get('/getOffChainData/:dataID', (req, res) => {
 
 //GET ROUTES
 app.get('/getItem/:itemID', (req, res) => {
-  network.getItem(req.params.itemID)
+  /*network.getItem(req.params.itemID)
     .then((response) => {
         let object = JSON.parse(response);
         res.send(JSON.stringify({ status: 'success', result: object}));
-    }).catch(err => res.send(JSON.stringify({ status: 'error', message: err })));
+    }).catch(err => res.send(JSON.stringify({ status: 'error', message: err })));*/
+    res.send(JSON.stringify({"status":"success","result":{"batchID":"1","dateOfDelivery":"01022020","dateOfDistribution":"DCI1","distributionCenterID":"01022020","packageID":"PI1","storeID":"SI11"}}))
 });
 
 app.post('/registerBatch', (req, res) => {
-  network.registerBatch(req.body.batchID, req.body.foodID, req.body.farmID, req.body.lotNo, req.body.dateOfHarvest)
-    .then((response) => {
-        res.send(JSON.stringify({ status: 'registerBatch - success', result: response }));
-    }).catch(err =>
-      res.send(JSON.stringify({ status: 'registerBatch - error', message: err })));
+  // network.registerBatch(req.body.batchID, req.body.foodID, req.body.farmID, req.body.lotNo, req.body.dateOfHarvest)
+  //   .then((response) => {
+  //       res.send(JSON.stringify({ status: 'registerBatch - success', result: response }));
+  //   }).catch(err =>
+  //     res.send(JSON.stringify({ status: 'registerBatch - error', message: err })));
+
+    res.send(JSON.stringify({ status: 'registerBatch - success', result: "Transaction submitted" }));
 });
 
 //POST ROUTES
 app.post('/registerPackage', (req, res) => {
-  network.registerPackage(req.body.packageID, req.body.batchID)
-    .then((response) => {
-        res.send(JSON.stringify({ status: 'registerPackage - success', result: response }));
-    }).catch(err =>
-      res.send(JSON.stringify({ status: 'registerPackage - error', message: err })));
+  // network.registerPackage(req.body.packageID, req.body.batchID)
+  //   .then((response) => {
+  //       res.send(JSON.stringify({ status: 'registerPackage - success', result: response }));
+  //   }).catch(err =>
+  //     res.send(JSON.stringify({ status: 'registerPackage - error', message: err })));
+  res.send(JSON.stringify({ status: 'registerPackage - success', result: "Transaction submitted" }));
 });
 
 
 app.post('/registerFoodFromFarmToPackageHouse', (req, res) => {
-  network.registerFoodFromFarmToPackageHouse(req.body.batchID, req.body.packagingHouseID, req.body.dateOfPackaging)
-    .then((response) => {
-        res.send(JSON.stringify({ status: 'registerFoodFromFarmToPackageHouse - success', result: response }));
-    }).catch(err =>
-      res.send(JSON.stringify({ status: 'registerFoodFromFarmToPackageHouse - error', message: err })));
+  // network.registerFoodFromFarmToPackageHouse(req.body.batchID, req.body.packagingHouseID, req.body.dateOfPackaging)
+  //   .then((response) => {
+  //       res.send(JSON.stringify({ status: 'registerFoodFromFarmToPackageHouse - success', result: response }));
+  //   }).catch(err =>
+  //     res.send(JSON.stringify({ status: 'registerFoodFromFarmToPackageHouse - error', message: err })));
+  res.send(JSON.stringify({ status: 'registerFoodFromFarmToPackageHouse - success', result: "Transaction submitted" }));
 });
 
 app.post('/registerFromPackageHouseToDistributionCenter', (req, res) => {
-  network.registerFromPackageHouseToDistributionCenter(req.body.packageID, req.body.distributionCenterID, req.body.dateOfDistribution)
-    .then((response) => {
-        res.send(JSON.stringify({ status: 'registerFromPackageHouseToDistributionCenter - success', result: response }));
-    }).catch(err =>
-      res.send(JSON.stringify({ status: 'registerFromPackageHouseToDistributionCenter - error', message: err })));
+  // network.registerFromPackageHouseToDistributionCenter(req.body.packageID, req.body.distributionCenterID, req.body.dateOfDistribution)
+  //   .then((response) => {
+  //       res.send(JSON.stringify({ status: 'registerFromPackageHouseToDistributionCenter - success', result: response }));
+  //   }).catch(err =>
+  //     res.send(JSON.stringify({ status: 'registerFromPackageHouseToDistributionCenter - error', message: err })));
+
+      res.send(JSON.stringify({ status: 'registerFromPackageHouseToDistributionCenter - success', result: "Transaction submitted" }));
 });
 
 app.post('/registerFromDistributionCenterToStore', (req, res) => {
-  network.registerFromDistributionCenterToStore(req.body.packageID, req.body.storeID, req.body.dateOfDelivery)
-    .then((response) => {
-        res.send(JSON.stringify({ status: 'registerFromDistributionCenterToStore - success', result: response }));
-    }).catch(err =>
-      res.send(JSON.stringify({ status: 'registerFromDistributionCenterToStore - error', message: err })));
+  // network.registerFromDistributionCenterToStore(req.body.packageID, req.body.storeID, req.body.dateOfDelivery)
+  //   .then((response) => {
+  //       res.send(JSON.stringify({ status: 'registerFromDistributionCenterToStore - success', result: response }));
+  //   }).catch(err =>
+  //     res.send(JSON.stringify({ status: 'registerFromDistributionCenterToStore - error', message: err })));
+
+  res.send(JSON.stringify({ status: 'registerFromDistributionCenterToStore - success', result: "Transaction submitted" }));
 });
 
 
